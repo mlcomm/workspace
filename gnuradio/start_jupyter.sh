b396f8730ba70ee02e75182c29c7b9d1f8e431d7
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-30 04:35:03 -0700

    Update start_jupyter.sh

--- gnuradio/start_jupyter.sh
+++ gnuradio/start_jupyter.sh
@@ -2,4 +2,4 @@
 
 jupyter notebook --generate-config
 cp jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
-jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root &
+jupyter notebook --ip=0.0.0.0 --port=8888 --notebook-dir=/opt --allow-root &
