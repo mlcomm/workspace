843a9fd49c4ec1f26bd7eafbb4ed7e9cac97bb3e
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-07-30 04:34:42 -0700

    Update start_jupyter.sh

--- tensorflow/start_jupyter.sh
+++ tensorflow/start_jupyter.sh
@@ -3,4 +3,4 @@
 
 jupyter notebook --generate-config
 cp jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
-jupyter notebook --ip=0.0.0.0 --port=8080 --allow-root &
+jupyter notebook --ip=0.0.0.0 --port=8080 --notebook-dir=/opt --allow-root &
