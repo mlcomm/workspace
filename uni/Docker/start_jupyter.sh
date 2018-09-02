6ab693e247974bdeead00b53033cfc300627577f
mlcomm <36723748+mlcomm@users.noreply.github.com>
2018-09-02 09:51:26 -0700

    Delete start_jupyter.sh

--- uni/Docker/start_jupyter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-jupyter notebook --generate-config
-cp -i jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
-jupyter notebook --ip=0.0.0.0 --port=8080 --notebook-dir=/ --allow-root &
